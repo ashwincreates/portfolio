65ed503f7f7a159888d1624cb851ef3d722bd021
ashwin <ashwin@pop-os.localdomain>
2021-12-20 12:56:25 +0530

    final test

--- next.config.js
+++ next.config.js
@@ -1,8 +1,6 @@
-const isProd = process.env.NODE_ENV === 'production'
-
 module.exports = {
-  assetPrefix: isProd ? '/portfolio/' : '',
-	basePath: isProd ? '/portfolio' : '',
+  assetPrefix:'/portfolio/',
+	basePath:'/portfolio',
   images: {
     loader: 'akamai',
     path: '',
