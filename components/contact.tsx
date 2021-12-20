996e3dc7708aca1da0b3df588352a94a6be7585e
ashwin <ashwin@pop-os.localdomain>
2021-12-20 11:21:09 +0530

    update

--- components/contact.tsx
+++ components/contact.tsx
@@ -1,12 +1,85 @@
-import { Fragment, useState } from "react";
+import { Fragment, useState, useEffect } from "react";
 import { HiOutlineMail } from "react-icons/hi";
+import {FiXOctagon, FiCheckCircle} from 'react-icons/fi';
 import { BiPaperPlane } from "react-icons/bi";
-import { useForm, ValidationError } from "@formspree/react";
 import { Dialog, Transition } from "@headlessui/react";
 
 function Contact() {
 	const [isOpen, setIsOpen] = useState(false);
-	const [state, handleSubmit] = useForm("mjvlvzya");
+	const [inputs, setInputs] = useState({
+		email: "",
+		message: ""
+	})
+	const [serverState, setServerState] = useState({
+    submitting: false,
+    status: null
+  });
+	const [fieldErrors, setFieldErrors] = useState({});
+	const validationRules = {
+    email: val => val && /^[^\s@]+@[^\s@]+\.[^\s@]+$/.test(val),
+    message: val => !!val // similar to "required"
+  };
+	const validate = () => {
+    let errors = {};
+    let hasErrors = 0;
+    for (let key of Object.keys(inputs)) {
+      errors[key] = !validationRules[key](inputs[key]);
+      hasErrors |= errors[key];
+    }
+    setFieldErrors(prev => ({ ...prev, ...errors }));
+    return !hasErrors;
+  };
+	const renderFieldError = field => {
+    if (fieldErrors[field]) {
+      return <p className="text-rose-500 mt-2">Please enter a valid {field}.</p>;
+    }
+  };
+	const handleServerResponse = (ok, msg) => {
+    setServerState({
+      submitting: false,
+      status: { ok, msg }
+    });
+    if (ok) {
+			setFieldErrors({});
+      setInputs({
+        email: "",
+        message: ""
+      });
+    }
+  };
+  const handleOnSubmit = event => {
+    event.preventDefault();
+		if (!validate()) {
+      return;
+    }
+    setServerState({ submitting: true, status: serverState.status });
+    fetch("https://formspree.io/f/mjvlvzya", {
+      method: "POST",
+      body: JSON.stringify(inputs)
+    })
+      .then(r => {
+        	handleServerResponse(true, "Thanks!, your message has been sent");
+      })
+      .catch(r => {
+        	handleServerResponse(false, "Error Occured!");
+      });
+		setIsOpen(false);
+  };
+	const handleOnChange = event => {
+    event.persist();
+    setInputs(prev => ({
+      ...prev,
+      [event.target.id]: event.target.value
+    }));
+  };
+
+	useEffect(() => {
+    // Only perform interactive validation after submit
+    if (Object.keys(fieldErrors).length > 0) {
+      validate();
+    }
+  }, [inputs]);
+
 	return (
 		<main
 			id="contact"
@@ -37,6 +110,11 @@ function Contact() {
 							<BiPaperPlane className="mr-2 text-white" size={24} />
 							Pin me up!
 						</a>
+						{serverState.status?<>
+							<div className={`py-2 mt-4 rounded-md px-3 flex items-center justify-center gap-x-2 ${serverState.status.ok?"bg-green-200 text-green-900":"bg-rose-200 text-rose-900"} font-bold`}>
+								{serverState.status.ok?<FiCheckCircle size={16}/>:<FiXOctagon size={16}/>}{serverState.status.msg}
+							</div>
+						</>:""}
 						<Transition appear show={isOpen} as={Fragment}>
 							<Dialog
 								as="div"
@@ -78,36 +156,29 @@ function Contact() {
 												Quick Message
 											</Dialog.Title>
 											<div className="mt-4">
-												<form onSubmit={handleSubmit}>
+												<form onSubmit={handleOnSubmit} noValidate>
 													<input
 														id="email"
 														name="email"
 														type="email"
+														onChange={handleOnChange}
 														placeholder="Email Address"
-														className="text-sm w-full outline-2 mb-4 bg-white outline-indigo-600 border-2 rounded-lg px-2 py-2"
+														className={`text-sm w-full outline-2 dark:text-gray-900 bg-white outline-indigo-600 border-2 ${fieldErrors["email"]?"border-rose-500":""} rounded-lg px-2 py-2`}
 													/>
-													<ValidationError
-														prefix="Email"
-														field="email"
-														errors={state.errors}
-													/>
+													  {renderFieldError("email")}
 													<textarea
 														id="message"
 														name="message"
+														onChange={handleOnChange}
 														rows={10}
 														placeholder="Message"
-														className="text-sm resize-none w-full mb-4 bg-white outline-2 outline-indigo-600 border-2 rounded-lg px-2 py-2"
+														className="text-sm resize-none w-full mt-4 bg-white dark:text-gray-900 outline-2 outline-indigo-600 border-2 rounded-lg px-2 py-2"
 													/>
-												</form>
-												<ValidationError
-													prefix="Message"
-													field="message"
-													errors={state.errors}
-												/>
+													  {renderFieldError("message")}
 												<button
 													type="submit"
-													disabled={state.submitting}
-													className="inline-flex mr-3 justify-center px-4 py-2 text-sm font-medium text-white bg-indigo-500 border border-transparent rounded-md hover:bg-indigo-600 focus:outline-none focus-visible:ring-2 focus-visible:ring-offset-2 focus-visible:ring-indigo-500"
+													disabled={serverState.submitting}
+													className="inline-flex mr-3 justify-center mt-4 px-4 py-2 text-sm font-medium text-white bg-indigo-500 border border-transparent rounded-md hover:bg-indigo-600 focus:outline-none focus-visible:ring-2 focus-visible:ring-offset-2 focus-visible:ring-indigo-500"
 												>
 													Send
 												</button>
@@ -116,10 +187,11 @@ function Contact() {
 														setIsOpen(false);
 													}}
 													type="button"
-													className="inline-flex justify-center px-4 py-2 text-sm font-medium text-indigo-900 bg-indigo-100 border border-transparent rounded-md hover:bg-indigo-200 focus:outline-none focus-visible:ring-2 focus-visible:ring-offset-2 focus-visible:ring-indigo-500"
+													className="inline-flex justify-center mt-4 px-4 py-2 text-sm font-medium text-indigo-900 bg-indigo-100 border border-transparent rounded-md hover:bg-indigo-200 focus:outline-none focus-visible:ring-2 focus-visible:ring-offset-2 focus-visible:ring-indigo-500"
 												>
 													Cancel
 												</button>
+												</form>
 											</div>
 										</div>
 									</Transition.Child>
